b29b5b13cd2c30fb92086d2cc579c9df011e14bc
Давидчук_ИВ <igordav@ukr.net>
2015-04-08 17:09:45 +0300

    Gotovo2

--- Hillel/src/HomeWork01.java
+++ Hillel/src/HomeWork01.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
  */
 public class HomeWork01 {
     public static void main(String[] params) {
-        Input();
+        Input();  // ввод данных
     }
     public static void Input () {
         double area;
         double perimetr;
-        String StrSquare = "Input Area: ";
-        String StrPerimetr = "Input Perimetr: ";
+        String strSquare = "Input Area: ";
+        String strPerimetr = "Input Perimetr: ";
         Scanner input = new Scanner(System.in);
-        System.out.println(StrSquare);
+        System.out.println(strSquare);
         area = input.nextDouble();
 
-        System.out.println(StrPerimetr);
+        System.out.println(strPerimetr);
         perimetr = input.nextDouble();
-        calculation(area,perimetr);
+        calculation(area,perimetr); // расчет значений
     }
     public static void calculation(Double area,Double perimetr){
         double length;
@@ -28,7 +28,8 @@ public class HomeWork01 {
         if (discriminant < 0 ){
             System.out.println("No solution");}
         else if (discriminant == 0){
-            length = width = perimetr / 4;
+            length = perimetr / 4;
+            width = length;
             System.out.println("This is square length and width = "+ length );}
         else {
             width = ((perimetr / 2) + Math.sqrt(discriminant)) / 2;
