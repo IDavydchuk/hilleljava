1fbf4c71997dbdf5d598363cc9a29a0c44eecbac
Давидчук_ИВ <igordav@ukr.net>
2015-04-09 11:04:35 +0300

    Gotovo 3

--- Hillel/src/HomeWork02.java
+++ Hillel/src/HomeWork02.java
@@ -11,28 +11,17 @@ public class HomeWork02 {
         String input = scaner.nextLine();
         char myKey = '7';
 
-        String myDecodeString = cryptString(input, myKey);
-        System.out.println("myDecodeString \n" + myDecodeString);
-        String myIncodeString = cryptString(myDecodeString, myKey);
-        System.out.println("\nmyIncodeString \n" + myIncodeString);
+        String myIncodeString = cryptString(input, myKey);
+        System.out.println("myIncodeString \n" + myIncodeString);
+        String myDecodeString = cryptString(myIncodeString, myKey);
+        System.out.println("\nmyDecodeString \n" + myDecodeString);
     }
     public static String cryptString(String strtingMessage, char key){
         char[] chars = strtingMessage.toCharArray();
         //System.out.println(Arrays.toString(chars));
-        //incoding
         for (int i=0; i< chars.length; i++){
             chars[i] ^= key;}
-        //System.out.println(Arrays.toString(chars));
         String someString = String.copyValueOf(chars);
         return someString;
     }
-    /*
-    public static String StringDeCrypt (String strtingMessage, char key ){
-        char[] chars = strtingMessage.toCharArray();
-        //decoding
-        for (int i=0; i< chars.length; i++)
-            chars[i] ^= key;
-        String someString = String.copyValueOf(chars);
-        return someString;
-    }*/
-}
+  }
