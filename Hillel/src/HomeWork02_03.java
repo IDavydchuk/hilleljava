a19018603e0ff6910769288196418d92d17db0bc
Давидчук_ИВ <igordav@ukr.net>
2015-04-09 20:59:20 +0300

    No My

--- Hillel/src/HomeWork02_03.java
+++ Hillel/src/HomeWork02_03.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.IntSummaryStatistics;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Давидчук_ИВ on 09/04/15.
@@ -17,69 +14,72 @@ public class HomeWork02_03 {
         System.out.println("Input string for decoding");
         Scanner scaner = new Scanner(System.in);
         String input = scaner.nextLine();
-        int myKey[];
-        myKey = createMyKey(arraySize);
-        System.out.println("myArray " + Arrays.toString(myKey));
-        myKey = sortMyArrayBooble(myKey);
-        System.out.println("myArraySort " + Arrays.toString(myKey));
+        int Key[];
+        Key = createKey(arraySize);
+        System.out.println("Array " + Arrays.toString(Key));
+        Key = sortByBubbleSort(Key);
+        System.out.println("Array bubble sort " + Arrays.toString(Key));
 
-        myKey = sortMyArrayChoice(myKey);
-        System.out.println("myArrayChoice " + Arrays.toString(myKey));
+        Key = sortBySelectionSort(Key);
+        System.out.println("Array selection sort " + Arrays.toString(Key));
 
-        String myEncodeString = cryptString(input, myKey);
-        System.out.println("myIncodeString \n" + myEncodeString);
-        String myDecodeString = cryptString(myEncodeString, myKey);
-        System.out.println("\nmyDecodeString \n" + myDecodeString);
+        String EncodeString = cryptString(input, Key);
+        System.out.println("Incode string \n" + EncodeString);
+        String DecodeString = cryptString(EncodeString, Key);
+        System.out.println("\nDecode string \n" + DecodeString);
     }
 
-    public static int[] sortMyArrayChoice(int[] myArray) {
-        int myIndex;
-        for (int i = 0; i < myArray.length; i++) {
-            int temp = myArray[i];
-            myIndex = getIndexOfMinElement(myArray, i, temp);
-            if (myIndex != i) {
-                myArray[i] = myArray[myIndex];
-                myArray[myIndex] = temp;
+    public static int[] sortBySelectionSort(int[] Array) {
+        int maxValueIndex;
+        for (int i = 0; i < Array.length; i++) {
+            maxValueIndex = getIndexOfMinElement(Array, i);
+            if (maxValueIndex != i) {
+                int temp = Array[i];
+                Array[i] = Array[maxValueIndex];
+                Array[maxValueIndex] = temp;
             }
         }
-        return myArray;
+        return Array;
     }
 
-    public static int getIndexOfMinElement(int[] myArray, int startFrom, int temp) {
-        int max = 0;
+    public static int getIndexOfMinElement(int[] Array, int startFrom) {
+        int max = -2147483648;
         int indexMax = startFrom;
-        for (int i = startFrom; i < myArray.length; i++) {
-            if (myArray[i] > max) {
-                max = myArray[i];
+        for (int i = startFrom; i < Array.length; i++) {
+            if (Array[i] > max) {
+                max = Array[i];
                 indexMax = i;
             }
         }
         return indexMax;
     }
 
-    public static int[] createMyKey(int arraySize) {
+    public static int[] createKey(int arraySize) {
         Random random = new Random();
-        int myArray[];
-        myArray = new int[arraySize];
-        for (int j = 0; j < myArray.length; j++) {
-            myArray[j] = random.nextInt(10);
+        int Array[];
+        Array = new int[arraySize];
+        for (int j = 0; j < Array.length; j++) {
+            Array[j] = random.nextInt(10);
         }
-        return myArray;
+        return Array;
     }
 
-    public static int[] sortMyArrayBooble(int[] myArray) {
-        for (int i = myArray.length - 1; i >= 2; i--) {
+    public static int[] sortByBubbleSort(int[] Array) {
+        for (int i = Array.length - 1; i >= 2; i--) {
             boolean sorted = true;
             for (int j = 0; j < i; j++) {
-                if (myArray[j] > myArray[j + 1]) {
-                    int temp = myArray[j];
-                    myArray[j] = myArray[j + 1];
-                    myArray[j + 1] = temp;
+                if (Array[j] > Array[j + 1]) {
+                    int temp = Array[j];
+                    Array[j] = Array[j + 1];
+                    Array[j + 1] = temp;
                     sorted = false;
                 }
             }
+            if (sorted) {
+                break;
+            }
         }
-        return myArray;
+        return Array;
     }
 
     public static String cryptString(String strtingMessage, int[] key) {
