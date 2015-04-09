4e9bca6aa506779ad31c34401dfc4c75f04c7ae2
Давидчук_ИВ <igordav@ukr.net>
2015-04-09 18:23:56 +0300

    + Choice

--- Hillel/src/HomeWork02_03.java
+++ Hillel/src/HomeWork02_03.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * Created by Давидчук_ИВ on 09/04/15.
  */
 public class HomeWork02_03 {
-    public static void main(String[] params ){
+    public static void main(String[] params) {
         // Shifr
         int arraySize;//
         System.out.println("Input size array");
@@ -19,28 +19,59 @@ public class HomeWork02_03 {
         String input = scaner.nextLine();
         int myKey[];
         myKey = createMyKey(arraySize);
-        System.out.println("myArray "+Arrays.toString(myKey));
+        System.out.println("myArray " + Arrays.toString(myKey));
         myKey = sortMyArrayBooble(myKey);
-        System.out.println("myArraySort "+Arrays.toString(myKey));
+        System.out.println("myArraySort " + Arrays.toString(myKey));
+
+        myKey = sortMyArrayChoice(myKey);
+        System.out.println("myArrayChoice " + Arrays.toString(myKey));
 
         String myEncodeString = cryptString(input, myKey);
         System.out.println("myIncodeString \n" + myEncodeString);
         String myDecodeString = cryptString(myEncodeString, myKey);
         System.out.println("\nmyDecodeString \n" + myDecodeString);
     }
-    public static int[] createMyKey(int arraySize){
+
+    public static int[] sortMyArrayChoice(int[] myArray) {
+        int myIndex;
+        for (int i = 0; i < myArray.length; i++) {
+            int temp = myArray[i];
+            myIndex = getIndexOfMinElement(myArray, i, temp);
+            if (myIndex != i) {
+                myArray[i] = myArray[myIndex];
+                myArray[myIndex] = temp;
+            }
+        }
+        return myArray;
+    }
+
+    public static int getIndexOfMinElement(int[] myArray, int startFrom, int temp) {
+        int max = 0;
+        int indexMax = startFrom;
+        for (int i = startFrom; i < myArray.length; i++) {
+            if (myArray[i] > max) {
+                max = myArray[i];
+                indexMax = i;
+            }
+        }
+        return indexMax;
+    }
+
+    public static int[] createMyKey(int arraySize) {
         Random random = new Random();
         int myArray[];
         myArray = new int[arraySize];
-        for (int j=0; j < myArray.length; j++){
-            myArray[j]= random.nextInt(10);}
+        for (int j = 0; j < myArray.length; j++) {
+            myArray[j] = random.nextInt(10);
+        }
         return myArray;
     }
-    public static int[] sortMyArrayBooble(int[] myArray){
+
+    public static int[] sortMyArrayBooble(int[] myArray) {
         for (int i = myArray.length - 1; i >= 2; i--) {
             boolean sorted = true;
             for (int j = 0; j < i; j++) {
-                if (myArray[j] > myArray[j+1]) {
+                if (myArray[j] > myArray[j + 1]) {
                     int temp = myArray[j];
                     myArray[j] = myArray[j + 1];
                     myArray[j + 1] = temp;
@@ -50,14 +81,15 @@ public class HomeWork02_03 {
         }
         return myArray;
     }
-    public static String cryptString(String strtingMessage, int[] key){
+
+    public static String cryptString(String strtingMessage, int[] key) {
         char[] chars = strtingMessage.toCharArray();
         int incr;
         //System.out.println(Arrays.toString(chars));
-        for (int i=0; i< chars.length; i++){
+        for (int i = 0; i < chars.length; i++) {
             incr = i % key.length;
             chars[i] ^= key[incr];
-            }
+        }
         String someString = String.copyValueOf(chars);
         return someString;
     }
